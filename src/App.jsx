94abe72f3e6d328c035db1ada1593d2c49ed7d05
Aselemi Divine <aselemidivine@gmail.com>
2022-12-20 16:00:29 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Home from "./pages/Home";
+// import ProductList from "../src/pages/ProductList";
+// import Register from "./pages/Register";
+// import ProductPage from "./pages/ProductPage";
+
+// import Cart from "./pages/Cart";
+// import Cart from "../src/pages/Cart";
+
+// import Login from "../src/pages/Login";
+
+const App = () => {
+  return (
+    <Home />
+    // <ProductList />
+    // <ProductPage />
+    // <Register />
+    // <Login />
+    // <Cart />
+  );
+};
+
+export default App;
